c9b47475459be6073e880ffa436c9415e96c9ea6
ajwong0105 <89656321+ajwong0105@users.noreply.github.com>
2021-09-16 00:50:41 -0500

    Following Tutorial
    Up to adding states

---
+++ state4.js
@@ -0,0 +1,9 @@
+demo.state4 = function(){};
+demo.state4.prototype = {
+    preload: function(){},
+    create: function(){
+        game.stage.backgroundColor ="#80FF80";
+        console.log('state4');
+    },
+    update: function(){}
+};
